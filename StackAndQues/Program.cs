2f4e5e651d69c4e34337491576a4ed64ba29a400
aayusharya1 <aayusharya07@gmail.com>
2020-10-30 09:09:54 +0530

    UC1_StackUsingLinkedList

---
+++ StackAndQues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue demo");
+            LinkedList StacklinkedList = new LinkedList();
+            StacklinkedList.Push(70);
+            StacklinkedList.Push(30);
+            StacklinkedList.Push(56);
+            StacklinkedList.Display();
+        }
+    }
+}
